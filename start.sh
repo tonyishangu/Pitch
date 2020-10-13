ca6246119f8438a4764f1b94a85f5b98a24cb38f
tonyishangu <tonyishangu@gmail.com>
2020-10-13 12:16:49 +0300

    add changes

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY=12345
 export MAIL_USERNAME=tonyishangu@gmail.com
 export MAIL_PASSWORD=Saige1ow
-export DATABASE_URL=postgres://etwgpmoauxfwpc:c2a6cc26047ee5715d6947e7cfe3629d28407212836e47c4249366e57b54ee87@ec2-54-165-164-38.compute-1.amazonaws.com:5432/degq7v28s514qb
+export DATABASE_URL=postgres://gyrvicqvwxjbpd:d2e271f0c7ff4fee7d456b16fcc8d6bed348e93278e2d5f6b55355ba6a2a2fd4@ec2-54-90-68-208.compute-1.amazonaws.com:5432/d3l2nq7cb3sdj4
 python3.8 manage.py server
